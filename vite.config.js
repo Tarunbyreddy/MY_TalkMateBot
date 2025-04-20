f04eac4c063e8828dccfe6f5769fa9393d89a500
Tarunbyreddy <141487155+Tarunbyreddy@users.noreply.github.com>
2025-04-20 12:42:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MY_TalkMateBot/',  // 👈 IMPORTANT
+  base: '/MY_TalkMateBot/',
   plugins: [react()],
 })
